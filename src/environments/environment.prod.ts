59c1dab87e206cf74b13e1470849b510ae5c3cfb
ADRIanXDBV <adriansito_a@hotmail.com>
2023-04-24 08:53:54 -0500

    no mames

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAcI8h_z-XeUWmXp4E1eUFmDFOR49MYeAE",
+    authDomain: "arevaloa-2c843.firebaseapp.com",
+    projectId: "arevaloa-2c843",
+    storageBucket: "arevaloa-2c843.appspot.com",
+    messagingSenderId: "832526621781",
+    appId: "1:832526621781:web:d9699335d070a3fde9c8dc"
+  }
+};
